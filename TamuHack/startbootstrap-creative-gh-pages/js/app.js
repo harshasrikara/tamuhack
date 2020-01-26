dd60c46da7f2053aa2e21c0fce2883fb691effa1
Harsha Srikara <hxc170009@utdallas.edu>
2020-01-26 09:32:38 -0600

    reading from cloud firestore complete

---
+++ TamuHack/startbootstrap-creative-gh-pages/js/app.js
@@ -0,0 +1,26 @@
+
+
+// Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCTZ8IY1puUxN1tTfowcKFwas2BudAGN3I",
+//     authDomain: "tamuhack-266218.firebaseapp.com",
+//     databaseURL: "https://tamuhack-266218.firebaseio.com",
+//     projectId: "tamuhack-266218",
+//     storageBucket: "tamuhack-266218.appspot.com",
+//     messagingSenderId: "311910522342",
+//     appId: "1:311910522342:web:afe2014493d090d693ca27",
+//     measurementId: "G-HSHXYQSQ39"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+//   var db = firebase.firestore();
+
+//   db.collection("users").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//     });
+//   });
+// test.firestore.js
+
+console.log("hello world");
